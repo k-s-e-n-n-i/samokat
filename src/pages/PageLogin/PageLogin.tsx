ab09786d7ed253347f6b989e0b8225cae6102d12
k-s-e-n-n-i <ksu.xa.v.ya@gmail.com>
2023-05-13 17:41:17 +0700

    Форма авторизации

--- src/pages/PageLogin/PageLogin.tsx
+++ src/pages/PageLogin/PageLogin.tsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import './PageLogin.scss';
 import { Navigate } from 'react-router-dom';
 import { APIData } from '../../redux/services/APIDataProcessing';
+import Input from '../../components/Input/Input';
+import Button from '../../components/Button/Button';
+import { TypeInput } from '../../redux/services/GlobalFunctions';
 
 const PageLogin = () => {
   const [username, setUsername] = useState('');
@@ -26,28 +29,22 @@ const PageLogin = () => {
           APIData.login({ email: emailLogin, password: passwordLogin, setServerText });
         }}
       >
-        {/* <Input
-          placeholder="Email"
-          name="email"
-          onChange={(e) => setEmailLogin(e.target.value)}
-          className="page-login__input"
-        />
+        <Input placeholder="Email" name="email" onChange={(e: TypeInput) => setEmailLogin(e.target.value)} />
         <Input
           placeholder="Пароль"
           type="password"
-          onChange={(e) => setPasswordLogin(e.target.value)}
-          className="page-login__input"
+          onChange={(e: TypeInput) => setPasswordLogin(e.target.value)}
         />
-        <Button type="submit">OK</Button> */}
+        <Button text="Войти" />
       </form>
-      <form
+      {/* <form
         className="page-login__form"
         onSubmit={(e) => {
           e.preventDefault();
           APIData.register({ username, password, email, setServerText });
         }}
       >
-        {/* <Input
+         <Input
           placeholder="E-mail"
           name="email"
           onChange={(e) => setEmail(e.target.value)}
@@ -65,9 +62,9 @@ const PageLogin = () => {
           onChange={(e) => setPassword(e.target.value)}
           className="page-login__input"
         />
-        <Button type="submit">OK</Button> */}
-      </form>
-      ,{serverText ? <p dangerouslySetInnerHTML={{ __html: serverText }}></p> : null}
+        <Button type="submit">OK</Button> 
+      </form> */}
+      {serverText ? <p dangerouslySetInnerHTML={{ __html: serverText }}></p> : null}
     </div>
   );
 };
